5dd798241408d6173d718be860939dd519e29e38
MBoto <m.botond07@gmail.com>
2023-12-18 12:18:59 +0100

    main.js - clearInput functon és add function hozzáadása

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 let szamok = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("add").addEventListener("click", add);
+});
+
+function clearInputs(){
+    for (let i = 0; i < document.getElementsByTagName("input").length; i++) {
+        document.getElementsByTagName("input")[i].value = null;
+    }
+}
+
+function add(){
+    if(document.getElementById("n").value != null){
+        szamok.push(document.getElementById("n").value);
+    }
+    clearInputs();
+}
