d2abbacd0273de08e59574d9e6097531ddf1144f
MBoto <m.botond07@gmail.com>
2023-12-18 12:55:29 +0100

    min és max function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let szamok = [];
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("add").addEventListener("click", add);
+    document.getElementById("min").addEventListener("click", min);
+    document.getElementById("max").addEventListener("click", max);
 });
 
 function clearInputs(){
@@ -16,4 +18,27 @@ function add(){
         clearInputs();
         document.getElementById("output").textContent += `${szamok[szamok.length-1]} `;
     }
+    else{
+        console.log("anya");
+    }
+}
+
+function min(){
+    let min = szamok[0];
+    for (let i = 0; i < szamok.length; i++) {
+        if(min > szamok[i]){
+            min = szamok[i];
+        }
+    }
+    document.getElementById("minOut").textContent = `Min: ${min}`;
+}
+
+function max(){
+    let max = szamok[0];
+    for (let i = 0; i < szamok.length; i++) {
+        if(max < szamok[i]){
+            max = szamok[i];
+        }
+    }
+    document.getElementById("maxOut").textContent = `Max: ${max}`;
 }
